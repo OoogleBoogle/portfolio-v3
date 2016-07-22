7bf4118dd4ce12e04f92f0bf927d21548b05eb16
OoogleBoogle <simattfield@gmail.com>
2016-07-22 15:28:18 -0700

    Work examples styled

--- gulpfile.js
+++ gulpfile.js
@@ -73,6 +73,7 @@ gulp.task('watch', function() {
   gulp.watch('src/js/**/**/*.js', ['scripts']);
   gulp.watch('src/scss/*.scss', ['sass']);
   gulp.watch('src/index.html', ['html']);
+  gulp.watch('src/img/*', ['images']);
 });
 
 // start server
