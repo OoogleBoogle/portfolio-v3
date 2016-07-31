49f77190e943cda187af88e1f3b4e37bf3ec08f1
OoogleBoogle <simattfield@gmail.com>
2016-07-31 11:15:08 -0700

    housekeeping on iOS fallback

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var projects = document.querySelectorAll('.example');
   var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
   if (iOS) {
+    var projects = document.querySelectorAll('.example');
     for (var i = 0; i < projects.length; i++) { 
       projects[i].addEventListener('click', function() {}); // iOS fallback for hover effects
     }
