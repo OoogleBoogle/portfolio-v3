25b069faf4c715fc337815319c144d5834282437
OoogleBoogle <simattfield@gmail.com>
2016-08-01 08:04:36 -0700

    patch for hover on iOS devices

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
   var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
   if (iOS) {
     var projects = document.querySelectorAll('.example');
-    for (var i = 0; i < projects.length; i++) { 
-      projects[i].addEventListener('click', function() {}); // iOS fallback for hover effects
+    for (var i = 0; i < projects.length; i++) { // iOS fallback for hover effects
+      projects[i].addEventListener('click', function() {
+        for (var i = 0; i < projects.length; i++) {
+          projects[i].classList.remove('iOS-Fallback');
+        }
+        this.classList.add('iOS-Fallback');
+      }); 
     }
   }
-})
+});
