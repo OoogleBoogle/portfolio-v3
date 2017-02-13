becf893df6552a0d412d20479ae5326ca194b5a7
OoogleBoogle <simattfield@gmail.com>
2017-02-12 18:45:49 -0800

    more script stuff

--- push_build.sh
+++ push_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ $BRANCH != 'master' ]
@@ -10,10 +10,10 @@ if [ $BRANCH != 'master' ]
 fi
 
 git checkout gh-pages
-git merge master -m "pre build merge" --ff-only
 if [ -a ./build ]
   then
     rm -f -r ./build
 fi
+git merge master -m "pre build merge" --ff-only
 gulp critical
 git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
