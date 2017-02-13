a2c865304bb67f07b8d94b90bd3d3e90a9b50484
OoogleBoogle <simattfield@gmail.com>
2017-02-12 16:44:06 -0800

    more stuff

--- push_build.sh
+++ push_build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-BRANCH=git rev-parse --abbrev-ref HEAD
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-git checkout master
-git merge $BRANCH -m "pulling in changes" --ff-only
+echo $BRANCH
 
-git checkout gh-pages
-git merge master -m "pre build merge" --ff-only
-if [ -a ./build ]
+if [ $BRANCH == 'optimizing' ]
   then
-    rm -f -r ./build
+    echo "Yay $BRANCH"
 fi
-gulp critical
-git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
+
+# git checkout master
+# git merge $BRANCH -m "pulling in changes" --ff-only
+
+# git checkout gh-pages
+# git merge master -m "pre build merge" --ff-only
+# if [ -a ./build ]
+#   then
+#     rm -f -r ./build
+# fi
+# gulp critical
+# git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
