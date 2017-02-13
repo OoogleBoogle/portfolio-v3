18ef2227982aaafb5c919b4ee53789469a6b743a
OoogleBoogle <simattfield@gmail.com>
2017-02-12 16:36:45 -0800

    update script

--- push_build.sh
+++ push_build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 git checkout gh-pages
-# git merge master -m "pre build merge" --ff-only
+git merge master -m "pre build merge" --ff-only
 if [ -a ./build ]
   then
     rm -f -r ./build
-    echo "build exists"
 fi
-# gulp critical
-# git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
+gulp critical
+git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
