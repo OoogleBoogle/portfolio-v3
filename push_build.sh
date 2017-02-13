603d8c5354d1f175d4853e8b00eb723b6421f63f
OoogleBoogle <simattfield@gmail.com>
2017-02-12 16:28:23 -0800

    updated gulp tasks

--- push_build.sh
+++ push_build.sh
@@ -2,5 +2,6 @@
 
 git checkout gh-pages
 git merge master -m "pre build merge" --ff-only
-gulp build
+rm -f -r ./build
+gulp critical
 git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
